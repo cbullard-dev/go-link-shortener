79754d90f5464d713419defae9eb1b3c978d2201
Cal <60569006+cbullard-dev@users.noreply.github.com>
2025-09-25 13:38:59 +0900

    Added the main function with a print line and a generateURLCode function that will generate the URL code from a string, Also added validation to make sure that the code does not already exist and if ti does try create the code again

--- main.go
+++ main.go
@@ -1,6 +1,25 @@
 package main
 
+import "fmt"
+
 // This project will be to understand the fundamentals in Go
 // The MVP for this project is it must accept a link URL, shorten the URL,
 // store the shortened and full URL, and then redirect a user navigating to the
 // shortened URL to the full URL
+
+var urlMap = make(map[string]string)
+
+func main(){
+	fmt.Println("Application started")
+}
+
+func generateURLCode(url string)string{
+	code:=""
+
+	// Check to confirm that the code is not present in the url map
+	_,ok := urlMap[code]
+	if ok {
+		code = generateURLCode(url)
+	}
+	return code
+}
