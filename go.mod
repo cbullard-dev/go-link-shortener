3d95bdd1bd835d3ef32ce80b115f8ef8a66fe004
Cal <60569006+cbullard-dev@users.noreply.github.com>
2025-09-25 14:43:34 +0900

    Added the go mod and go sum files using the 'go mod init' command. Also completed the function to create a new random code for the URL

---
+++ go.mod
@@ -0,0 +1,5 @@
+module cb-dev.com/link-shortener
+
+go 1.25.1
+
+require github.com/davecgh/go-spew v1.1.1 // indirect
--- main.go
+++ main.go
@@ -1,6 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"crypto/rand"
+	"fmt"
+	"math/big"
+	"os"
+
+	"github.com/davecgh/go-spew/spew"
+)
 
 // This project will be to understand the fundamentals in Go
 // The MVP for this project is it must accept a link URL, shorten the URL,
@@ -8,18 +15,27 @@ import "fmt"
 // shortened URL to the full URL
 
 var urlMap = make(map[string]string)
+const urlCodeLength = 8
 
 func main(){
-	fmt.Println("Application started")
+	temp := generateURLCode()
+	urlMap[temp] = os.Args[1]
+	fmt.Printf("The code is: %v\n", temp)
+	spew.Dump(urlMap)
 }
 
-func generateURLCode(url string)string{
+func generateURLCode()string{
 	code:=""
+	charsList:="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+	for range(urlCodeLength){
+		c, _ := rand.Int(rand.Reader,big.NewInt(int64(len(charsList))))
+		code += string(charsList[c.Int64()])
+	}
 
 	// Check to confirm that the code is not present in the url map
 	_,ok := urlMap[code]
 	if ok {
-		code = generateURLCode(url)
+		code = generateURLCode()
 	}
 	return code
 }
