3c3917ad64895a0ae327c2f4f7788efabfc0c73c
Cal <60569006+cbullard-dev@users.noreply.github.com>
2025-09-25 13:36:47 +0900

    Initial commit with main file, readme and gitignore

---
+++ README.md
@@ -0,0 +1,5 @@
+# URL Shortener Project
+
+This project will be to understand the fundamentals in Go
+
+The MVP for this project is it must accept a link URL, shorten the URL, store the shortened and full URL, and then redirect a user navigating to the shortened URL to the full URL
---
+++ main.go
@@ -0,0 +1,6 @@
+package main
+
+// This project will be to understand the fundamentals in Go
+// The MVP for this project is it must accept a link URL, shorten the URL,
+// store the shortened and full URL, and then redirect a user navigating to the
+// shortened URL to the full URL
