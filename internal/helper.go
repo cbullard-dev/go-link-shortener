4d9c2968c703cd09575b9c112f5e01578dc63208
Cal <60569006+cbullard-dev@users.noreply.github.com>
2025-10-03 19:09:58 +0900

    Updated the formatting of the file using the go fmt command to format the files before commit

--- internal/helper.go
+++ internal/helper.go
@@ -17,8 +17,8 @@ func GenerateUrlCode(urlCodeLength int) string {
 }
 
 func LoadData(fileName string, urlMap map[string]string) error {
-	file,err := os.Open(fileName)
-	if err != nil{
+	file, err := os.Open(fileName)
+	if err != nil {
 		return err
 	}
 
@@ -29,13 +29,13 @@ func LoadData(fileName string, urlMap map[string]string) error {
 }
 
 func SaveData(fileName string, urlMap map[string]string) error {
-	file, err := os.OpenFile(fileName,os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0644)
-	if err != nil{
+	file, err := os.OpenFile(fileName, os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0644)
+	if err != nil {
 		return err
 	}
 
 	defer file.Close()
 
-	encoder:=json.NewEncoder(file)
+	encoder := json.NewEncoder(file)
 	return encoder.Encode(urlMap)
 }
