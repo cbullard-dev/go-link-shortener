c3a4b90b48113db86462ad5cb5525293cb84e9d9
Cal <60569006+cbullard-dev@users.noreply.github.com>
2025-10-04 01:00:04 +0900

    Added new test for the SaveData function

--- internal/helper_test.go
+++ internal/helper_test.go
@@ -1,6 +1,10 @@
 package helper
 
 import (
+	"encoding/json"
+	"os"
+	"path/filepath"
+	"reflect"
 	"regexp"
 	"testing"
 )
@@ -18,5 +22,29 @@ func TestUrlGenerationFormat(t *testing.T) {
 	want, _ := regexp.Match("[a-zA-Z0-9]{8}", []byte(code))
 	if !want {
 		t.Errorf(`Generated code expected to match regex "[a-zA-Z0-9]{8}", actual length result: %q`, code)
+	}
+}
+
+func TestSaveData(t *testing.T) {
+	saveFileName := filepath.Join(t.TempDir(), "testSaveData.json")
+	testMap := map[string]string{"unique_key_one": "string_value_one", "unique_key_two": "string_value_two"}
+
+	SaveData(saveFileName, testMap)
+
+	_, err := os.Stat(saveFileName)
+
+	if os.IsNotExist(err) {
+		t.Errorf(`The save file does not exist: %q`, err)
+	}
+
+	data, err := os.ReadFile(saveFileName)
+
+	if err != nil {
+		t.Errorf(`Error reading the file: %q`, err)
+	}
+	loadedData := make(map[string]string)
+	json.Unmarshal([]byte(data), &loadedData)
+	if !reflect.DeepEqual(loadedData, testMap) {
+		t.Errorf(`Input data: %q, does not match the output data: %q`, testMap, loadedData)
 	}
 }
