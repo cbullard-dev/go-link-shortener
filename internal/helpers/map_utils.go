c04b99351dc0bd28df391b9c20296e08fbb8e1dd
Cal <60569006+cbullard-dev@users.noreply.github.com>
2025-10-08 14:15:59 +0900

    Updated formatting

--- internal/helpers/map_utils.go
+++ internal/helpers/map_utils.go
@@ -1,10 +1,10 @@
 package helpers
 
 func ContainsValue(m map[string]string, target string) bool {
-    for _, v := range m {
-        if v == target {
-            return true
-        }
-    }
-    return false
+	for _, v := range m {
+		if v == target {
+			return true
+		}
+	}
+	return false
 }
--- internal/helpers/map_utils_test.go
+++ internal/helpers/map_utils_test.go
@@ -3,11 +3,11 @@ package helpers
 import "testing"
 
 func TestContainsValue(t *testing.T) {
-    testMap := map[string]string{"key":"value"}
-    want := true
-    received := ContainsValue(testMap,"value")
+	testMap := map[string]string{"key": "value"}
+	want := true
+	received := ContainsValue(testMap, "value")
 
-    if received != want{
-        t.Errorf("Expected %v, Received %v", want, received)
-    }
+	if received != want {
+		t.Errorf("Expected %v, Received %v", want, received)
+	}
 }
